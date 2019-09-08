27fe7c4350da4004236c650b9aa99c09ccba606f
ARIJIT <ARIJIT@ARIJIT-PC>
2019-09-08 12:36:41 +0530

    adding code for: 1. Generate string with a,b,c where at most 1 b and 2 c can be present. 2. Binary string with 0, 1 without consecutive 1.

---
+++ src/main/java/org/arijit/string/combination/BinaryStringWithoutConsecutive1.java
@@ -0,0 +1,29 @@
+package org.arijit.string.combination;
+
+public class BinaryStringWithoutConsecutive1 {
+
+	private static void generate(int len, char data[], int index) {
+		if(index==len) {
+			System.out.println(data);
+			return;
+		}
+		data[index]='0';
+		generate(len, data, index+1);		
+		if(index==0) {
+			data[index]='1';
+			generate(len, data, index+1);
+		}
+		else if(index>0 && data[index-1]!='1') {
+			// we have made sure no consecutive 1 present.
+			data[index]='1';
+			generate(len, data, index+1);
+		}
+				
+	}
+	
+	public static void main(String args[]) {
+		int len = 3;
+		char data[] = new char[len];
+		generate(len, data, 0);
+	}
+}
---
+++ src/main/java/org/arijit/string/combination/GenerateString1.java
@@ -0,0 +1,43 @@
+package org.arijit.string.combination;
+
+public class GenerateString1 {
+
+	private static void generate(char[] cArray, int n, char[] data, int index, int bCount, int cCount, int maxB,
+			int maxC, int[] totalCount) {
+		if (index == n) {
+			System.out.println(data);
+			totalCount[0] = ++totalCount[0];
+			return;
+		}
+		for (int i = 0; i < cArray.length; i++) {
+			if (cArray[i] == 'b' && bCount >= maxB) {
+				continue;
+
+			} else if (cArray[i] == 'c' && cCount >= maxC) {
+				continue;
+
+			}
+			data[index] = cArray[i];
+			if (cArray[i] == 'b') {
+				// you should not do bCount ++ as it will change bCount. Now when after
+				// recursion this stack will again get control it will have increased b value
+				// even when no 'b' is encountered.
+				generate(cArray, n, data, index + 1, bCount + 1, cCount, maxB, maxC, totalCount);
+			} else if (cArray[i] == 'c') {
+				generate(cArray, n, data, index + 1, bCount, cCount + 1, maxB, maxC, totalCount);
+			} else {
+				generate(cArray, n, data, index + 1, bCount, cCount, maxB, maxC, totalCount);
+			}
+
+		}
+	}
+
+	public static void main(String args[]) {
+		char[] cArray = { 'a', 'b', 'c' };
+		int n = 3;
+		char[] data = new char[n];
+		int[] totalCount = new int[1];
+		generate(cArray, n, data, 0, 0, 0, 1, 2, totalCount);
+		System.out.println("Total String count: " + totalCount[0]);
+	}
+}
